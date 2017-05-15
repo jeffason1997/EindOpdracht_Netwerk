17124d778a8a6642088ff363306db4e15399fd3b
JeffreyLantinga <jeffreylantinga@hotmail.com>
2017-05-15 14:24:56 +0200

    boiy

---
+++ src/GUI.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+/**
+ * Created by Jeffrey Lantinga, 2101060 on 27-3-2017.
+ */
+public class GUI extends JFrame {
+    public GUI(){
+
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Jeffrey Lantinga, 2101060 on 27-3-2017.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        
+    }
+}
